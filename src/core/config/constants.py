82e5a7d54ad485ff51175c1cfd29580156e1c10c
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-09-19 22:15:49 +0200

    update: new constants values

--- src/core/config/constants.py
+++ src/core/config/constants.py
@@ -1,14 +1,19 @@
 from dataclasses import dataclass
 
 
-
+@dataclass(frozen=True)
+class RedisCacheExpiry:
+    """
+    Die dauer in Sekunden, wie lange Einträge in Redis gespeichert werden
+    """
+    timetable: int = 60 * 60 * 24 # 24 stunden
 
 @dataclass(frozen=True)
 class Constants:
     """
     Hardcodierte Systemkonstanten
     """
-    pass
+    redis_cache_expiry: RedisCacheExpiry = RedisCacheExpiry()
 
 
 CONSTANTS = Constants()
