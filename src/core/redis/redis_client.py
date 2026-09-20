d00080c8aac19aba76f3f909e48f418a8bcd77ad
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-09-20 12:40:59 +0200

    chore: reworked redis client and pipeline

--- src/core/redis/redis_client.py
+++ src/core/redis/redis_client.py
@@ -1,10 +1,15 @@
 from redis.asyncio import Redis
 
-redis_client = Redis(
-    host="localhost",
-    db=0,
-    decode_responses=True,
-)
+redis_client: Redis | None = None
 
-def get_redis_client():
+
+def create_redis_client():
+    global redis_client
+    redis_client = Redis(
+        host="localhost",
+        db=0,
+        decode_responses=True,
+    )
+
+def get_redis():
     return redis_client
--- src/core/redis/redis_pipeline.py
+++
@@ -1,8 +0,0 @@
-from core.redis.redis_client import get_redis_client
-
-
-async def get_pipeline():
-    client = get_redis_client()
-    async with client.pipeline(transaction=True) as pipe:
-        yield pipe
-        await pipe.execute()
